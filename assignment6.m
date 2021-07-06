79e183766186e4a60aa5064837f4749342538df9
Kadir Efe Dilek <71722234+kadirefe-dilek@users.noreply.github.com>
2021-07-06 10:07:13 +0300

    Add files via upload

---
+++ assignment6.m
@@ -0,0 +1,7 @@
+% Assignment 6: Write an m-function that takes "n" as an input and gives an,
+% "n x n" identity matrix as an output.
+
+n = input("Enter the desired size: "); % taking input from the user
+identity = eye(n); % creating and initializing the identity matrix
+
+disp(identity); % outputing the identity matrix
