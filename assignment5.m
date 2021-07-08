0089beee832110f749d43433f050582300dc1c0f
Kadir Efe Dilek <71722234+kadirefe-dilek@users.noreply.github.com>
2021-07-08 13:26:03 +0300

    Update assignment5.m

--- assignment5.m
+++ assignment5.m
@@ -1,15 +1,5 @@
-% Assignment 5: Take input from the user for three lengths and check if 
-% these lengths are able to denote a triangle. If so, state the type of the
-% triangle in sense of side lengths. (equilateral-isosceles-scalene)
-
-% defining function IsTriangle wich takes no input argument
-function [] = IsTriangle()
-
-    % Taking the length data and holding it in a vector
-    fprintf("Enter three lengths:\n")
-    len(1) = input("First length: ");
-    len(2) = input("Second length: ");
-    len(3) = input("Third length: ");
+% Defining the function IsTriangle which takes lengths as a vector and returns void
+function [] = IsTriangle(len)
 
     % checking for the length property of triangle 
     if (abs(len(1)+len(2)) >= abs(len(3)) && abs(len(1)+len(3)) >= abs(len(2)) && abs(len(2)+len(3)) >= abs(len(1)))
@@ -28,7 +18,9 @@ function [] = IsTriangle()
             fprintf("The triangle [");
             fprintf('%g, ', len(1:end-1));
             fprintf('%g] is an equilateral triangle.\n', len(end)); 
-        % if it is an isosceles triangle
+       
+
+       % if it is an isosceles triangle
         elseif len(1) == len(2) || len(1) == len(3) || len(2) == len(3)
             fprintf("The triangle [");
             fprintf('%g, ', len(1:end-1));
@@ -44,4 +36,4 @@ function [] = IsTriangle()
         fprintf('%g, ', len(1:end-1));
         fprintf('%g] does not denote a triangle.\n', len(end));
     end
-end 
+end
