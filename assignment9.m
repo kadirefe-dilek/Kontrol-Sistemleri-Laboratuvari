0f743baa960e3aff948fe29a770bca52aaaa9e48
Melikebu <75503418+Melikebu@users.noreply.github.com>
2021-07-11 17:31:25 +0300

    Add files via upload

--- assignment9.m
+++ assignment9.m
@@ -1,53 +1,38 @@
-% upper left white square
-x_w1 = [0 0 -4 -4];
-y_w1 = [0 4 4 0];
-s_white1 = [x_w1(:) y_w1(:)];
+%assignment 9 11-07-2021 
 
-% bottom left white square
-x_w2 = [0 0 -4 -4];
-y_w2 = [0 -4 -4 0];
-s_white2 = [x_w2(:) y_w2(:)];
+a=[0 2 2 0];
+b=[0 0 2 2];
+c=[0 4 4 0];
+d=[0 0 4 4];
+e=[0 1 1 0];
+f=[0 0 1 1];
+g=[-4 -4 4 4];
+h=[-4  4 4 -4];
+k=hgtransform;
+l=hgtransform;
+m=hgtransform;
+n=hgtransform;
 
-% bottom right white square
-x_w3 = [0 0 4 4];
-y_w3 = [0 -4 -4 0];
-s_white3 = [x_w3(:) y_w3(:)];
+patch("XData",c,"YData",d,"FaceColor","red",'EdgeColor','none',"Parent",k);
+patch("XData",a,"YData",b,"FaceColor","black",'EdgeColor','none',"Parent",l);
+patch("XData",e,"YData",f,'FaceColor',[0.5 0.5 0.5],'EdgeColor','none',"Parent",m);
+patch("XData",g,"YData",h,'FaceColor',"none",'EdgeColor','black',"Parent",n);
 
-% red square
-x_r = [0 0 4 4];
-y_r = [0 4 4 0];
-s_red = [x_r(:) y_r(:)];
+set(gca,'XTick',[], 'YTick', []);
 
-% gray square
-x_g = [0 0 1 1];
-y_g = [0 1 1 0];
-s_gray = [x_g(:) y_g(:)];
+ax = gca;
+ax.XAxisLocation = 'origin'
+ax.YAxisLocation = 'origin'
 
-% black square
-x_b = [0 0 2 2];
-y_b = [0 2 2 0];
-s_black = [x_b(:) y_b(:)];
 
-for t = 0:pi/180:2*pi
-    % Rotation matrix
-    R = [cos(t) -sin(t); sin(t) cos(t)];
-    % executing the rotation
-    s_black_rotate = s_black*R;
-    
-    % plotting
-    fill(s_white1(:,1), s_white1(:,2), "w")
-    hold on
-    fill(s_white2(:,1), s_white2(:,2), "w")
-    hold on
-    fill(s_white3(:,1), s_white3(:,2), "w")
-    hold on
-    fill(s_red(:,1), s_red(:,2), "r")
-    fill(s_black_rotate(:,1), s_black_rotate(:,2), "k")
-    hold on
-    fill(s_gray(:,1), s_gray(:,2), [0.5 0.5 0.5]);
-    hold off
 
-    % Animation can be done in different ways
-    %pause(0.01)    % waits 0.01 seconds between plottings
-    drawnow     % executes as fast as the computer can
+axis equal
+xlim([-4 4]);
+ylim([-4 4]);
+
+r1=2*pi;
+r2=0;
+for t=linspace(0,1,1000)
+    l.Matrix=makehgtform("zrotate",r1+t*(r2-r1));
+    drawnow
 end
